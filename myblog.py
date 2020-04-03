1cef18376606cc8fd1154967892bfd8a2d9319d9
Tianyang Li <teewhyli.tl@gmail.com>
2020-04-03 21:48:23 +1300

    finished layout content block

--- myblog.py
+++ myblog.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'TY Li',
+        'title': 'Blog Post 1',
+        'content': 'Hello World!',
+        'date_posted': 'April 20, 2020'
+    },
+    {
+        'author': 'Jenny Hsiao',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2020'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def homepage():
-    return "<h1>Homepage</h1>"
+    return render_template('home.html', posts=posts, title='Hey!')
 
 @app.route("/about")
 def about():
-    return "<h1>About page</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
