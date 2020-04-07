f5771a205ea3fe097503979393547f7df32212cc
Tianyang Li <teewhyli.tl@gmail.com>
2020-04-07 16:26:29 +1200

    finished database with sqlalchemy

--- myblog.py
+++ myblog.py
@@ -1,8 +1,35 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'aabcbb8bf275c9d1a503d6ab3793590d'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' # /// is relative path
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #turning off the warning, we're just using for practice.
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True) #lazy=True load only when necessary; backref is like "reversed" reference in sql.
+
+    def __repr__(self):
+        return f"User('{self.username}', {self.email}, {self.image_file})"
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default = datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [
     {
